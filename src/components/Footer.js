77e497ca8f68566819e275d400ba96b64e2b7873
Julian <julian.padurariu@me.com>
2021-11-23 09:35:16 +0000

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,7 +24,11 @@ export default () => (
         bottom:'0', 
         color: '#d8c69d',}} className="taCenter">
       © {new Date().getFullYear()} Toate drepturile rezervate. {' '}
-          <a href="https://aqua-bilbor.ro">Aqua Bilbor S.A.</a>
+          <a href="https://aqua-bilbor.ro">Aqua Bilbor S.R.L.</a> Societate cu Raspundere Limitată.<br></br>
+Sediu social: Sat Tunari, Comuna Tunari, Str. 1 Decembrie Nr. 144 - 148, Biroul Nr. 1, Etaj 1, Judeţul Ilfov.<br></br>
+Număr din registrul comerțului: J23/4278/2019.<br></br>
+Cod unic de înregistrare: 39818243.<br></br>
+Capital social: 10.284.000 lei
       </div>
     </footer>
   </div>
